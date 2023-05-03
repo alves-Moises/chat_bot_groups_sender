ab67553df9f542ecd7e59ef7a538fdab76dbe4d9
alves-Moises <moisesinho_ra@hotmail.com>
2023-05-03 01:39:30 -0300

    new: CheckDay && CheckIgnoreReacts functions

--- src/json_functions.js
+++ src/json_functions.js
@@ -36,6 +36,58 @@ const CheckGroupID = (group_id) => {
     return false
 }
 
+const CheckIgnoreReacts = (group_id) => {
+    const groupsFilePATH = path.resolve(__dirname, "./json/groups.json")
+    dados = JSON.parse(fs.readFileSync(groupsFilePATH))
+
+    if(dados.includes(group_id)){
+        return true
+    }
+
+    return false
+}
+
+
+
+
+const CheckDay = (file) => {
+    console.log(chalk.greenBright("Checkadno o dia..."))
+    const today = new Date()
+
+    dados_today = today.toString().split(" ")
+
+    day = dados_today[2]
+    month = GetMonth(dados_today[1])
+    year = dados_today[3]
+    today_formated = `["${day}/${month}/${year}"]`
+    
+    var filepath = `./json/${file}`
+    var dayPath = path.resolve(__dirname, filepath)
+    
+    dados_day = JSON.parse(fs.readFileSync(dayPath))
+    day_string = dados_day.toString()
+    day_array = day_string.split("/")  // dd mm yy
+    j_day = day_array[0]
+    j_month = GetMonth(day_array[1])
+    j_year = day_array[2]
+
+    // j_: json date
+    if(j_year < year){valid = true}
+    else if(j_month < month){valid = true}
+    else if(j_day < day){valid = true}
+    else(valid = false)
+    
+    if(valid){
+        fs.writeFileSync(dayPath, today_formated)
+        console.log(chalk.green("Novo dia"))
+        console.log(chalk.gray("Enviando mensagens..."))
+    }else{
+        console.log(chalk.redBright("Não foi necessário enviar as mensagns"))
+    }
+    
+    return valid
+}
+
 
 module.exports = { 
     GetGroupsArray, 
