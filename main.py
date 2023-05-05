4f1982688074759183d26719bd87333071109a60
alves-Moises <moisesinho_ra@hotmail.com>
2023-05-05 20:07:38 -0300

    update: main.py cleancode

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 
 print("Iniciando...")
-# os.system("cd D:\coding\Js\projetos pessoais\messagens_em_grupo\chat_bot_groups")
 os.system("cd /coding/Js/projetos_pessoais/messagens_em_grupo/chat_bot_groups/")
 
 action = f'["propagandati"]' if input("Digite 1 para propaganda Ti  ") == "1" else f'["propagandabot"]'
-
 os.system(f"echo {action} > src/json/open_action.json")
 
 print(f"Opção selecionada: {action}")
+
 os.system("node index.js")
 input("erro?")
