37290f3db1aecc9b382c7712402ffc5aa913ac2c
alves-Moises <moisesinho_ra@hotmail.com>
2023-10-17 22:40:53 -0300

    Update main.py: new init functions

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 print("Iniciando...")
 os.system("cd /coding/Js/projetos_pessoais/messagens_em_grupo/chat_bot_groups/")
 
-action = f'["propagandati"]' if input("Digite 1 para propaganda Ti  ") == "1" else f'["propagandabot"]'
+inpt = input("Digite\n1 para propaganda Ti\n2 para propaganda da vih  ")
+
+action = f'["propagandati"]' if inpt == "1" else  '["propagandavih"]'  if inpt == "2"  else f'["propagandabot"]'
 os.system(f"echo {action} > src/json/open_action.json")
 
 print(f"Opção selecionada: {action}")
 
-os.system("node index.js")
+os.system("node ./index.js")
 input("erro?")
