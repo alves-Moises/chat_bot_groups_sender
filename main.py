0e28868d97727ca7ac6a19f85a39eaf0747b4fbe
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-04 00:01:07 -0300

    update py file

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 print("Iniciando...")
 os.system("cd /coding/Js/projetos_pessoais/messagens_em_grupo/chat_bot_groups/")
 
-inpt = input("Digite\n1 para propaganda Ti\n2 para propaganda da vih  ")
+# inpt = input("Digite\n1 para propaganda Ti\n2 para propaganda da vih  ")
 
-action = f'["propagandati"]' if inpt == "1" else  '["propagandavih"]'  if inpt == "2"  else f'["propagandabot"]'
-os.system(f"echo {action} > ./src/json/open_action.json")
+# action = f'["propagandati"]' if inpt == "1" else  '["propagandavih"]'  if inpt == "2"  else f'["propagandabot"]'
+# os.system(f"echo {action} > ./src/json/open_action.json")
 
-print(f"Opção selecionada: {action}")
+# print(f"Opção selecionada: {action}")
 
 os.system("node ./index.js")
 input("erro?")
